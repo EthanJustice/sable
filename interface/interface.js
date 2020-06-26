981f737f710cd470e554d6a9c7b58474d6869092
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-06-25 21:32:12 -0400

    fix: better tagging system

--- interface/interface.js
+++ interface/interface.js
@@ -11,6 +11,12 @@ sable.start(document.body.querySelector('.tests'), {
 
 window.addEventListener('sable-change', data => {
 	data = data.detail;
+	
+	let change;
+	if (data.change == 'changed-attribute') {
+		change = `Changed attribute: [${data.attribute}=${data.old || '""'}] to [${data.attribute}=${data.new || '""'}]`
+	} else if (data.change == 'added-node') { change = `Added node ${data.children}` }
+	else { change = `Removed node ${data.children}` }
 
-	document.body.appendChild(buildElement('p', `${String(data.target.tagName).toLocaleLowerCase()}.${data.uniqueId}`, { className: `sable-interface ${data.change}` }));
+	document.body.querySelector('.stream').appendChild(buildElement('p', `${change} ---------- ${String(data.target.tagName).toLocaleLowerCase()}.${data.uniqueId}`, { className: `sable-interface ${data.change}` }));
 });
--- src/index.js
+++ src/index.js
@@ -2,18 +2,41 @@ class Sable {
 	constructor(options) {
 		const register = (data, observer) => {
 			data.forEach(record => {
+				if (record.attributeName == 'data-sable-id') { return }
+				let id = this.tracked.filter(item => item.element == record.target).map(item => item.id)[0] || this._unique();
+
+				if (!this.tracked.some(item => item.id == id)) {
+					this.tracked.push({ element: record.target, id: id });
+					record.target.dataset.sableId = id;
+				}
+
 				let recordData = {
 					change: false,
 					new: '',
 					old: '',
-					uniqueId: this._unique(),
-					target: record.target
+					attribute: '',
+					uniqueId: id,
+					target: record.target,
+					children: ''
 				};
 
-				if (record.addedNodes.length == 0 && record.removedNodes.length != 0) { recordData.change = 'removed-node' }
-				else if (record.addedNodes.length != 0 && record.removedNodes.length == 0) { recordData.change = 'added-node' }
-				else if (record.type == 'attributes') {
+				if (record.addedNodes.length == 0 && record.removedNodes.length != 0) {
+					recordData.change = 'removed-node';
+					record.removedNodes.forEach(node => {
+						this.tracked.filter(item => item.id == node.dataset.sableId).forEach((item, index) => delete this.tracked[index])
+						recordData.children += ` ${String(node.tagName).toLocaleLowerCase()}.${node.dataset.sableId}`
+					});
+				} else if (record.addedNodes.length != 0 && record.removedNodes.length == 0) {
+					recordData.change = 'added-node';
+					record.addedNodes.forEach(node => {
+						let childId = this._unique()
+						this.tracked.push({ element: node, id: childId });
+						node.dataset.sableId = childId
+						recordData.children += ` ${String(node.tagName).toLocaleLowerCase()}.${node.dataset.sableId}`
+					});
+				} else if (record.type == 'attributes') {
 					recordData.change = 'changed-attribute';
+					recordData.attribute = record.attributeName;
 					recordData.old = record.oldValue;
 					recordData.new = record.target[record.attributeName];
 				}
@@ -26,9 +49,10 @@ class Sable {
 			});
 		}
 
-		this.ids = [];
+		this.tracked = [];
 
 		this.observer = new MutationObserver(register);
+		this.latestConfig = {};
 
 		this.options = Object.assign({
 			// defaults
@@ -38,12 +62,15 @@ class Sable {
 	};
 
 	start(element, config) {
+		this.latestConfig = {
+			element: element,
+			config: config
+		};
 		this.observer.observe(element, config);
 	};
 
 	_unique() {
 		let id = Math.ceil(Math.random() * 9999999999);
-		this.ids.push(id);
 		return id;
 	};
 
